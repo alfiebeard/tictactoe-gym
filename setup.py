fd042387ecf27f7fcf8a86d6a2203b30b4c0d094
alfiebeard <alfiebeard96@gmail.com>
2022-10-12 21:22:25 +0100

    Project rename

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
       name='tictactoe_gym',
       version='0.1.0',
       description='An OpenAI gym environment for playing Tic Tac Toe',
-      url='https://github.com/alfiebeard/gym-tictactoe',
+      url='https://github.com/alfiebeard/tictactoe-gym',
       author='Alfie Beard',
       license='MIT License',
       install_requires=['gym']
