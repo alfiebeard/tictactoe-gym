11b3c378565b19134420310ddf90e670556aeaec
alfiebeard <alfiebeard96@gmail.com>
2022-10-13 18:27:28 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
       name='tictactoe_gym',
-      version='1.0.0',
+      version='1.0.1',
       description='An OpenAI gym environment for playing Tic Tac Toe',
       url='https://github.com/alfiebeard/tictactoe-gym',
       author='Alfie Beard',
